91b89b9ac39cc8a4c89b8bb1143bc0c148784064
downkhg <37098799+downkhg@users.noreply.github.com>
2018-04-23 12:51:13 +0900

    Add Unity Chan
    Unity chan asset

--- UnityTest/Assets/GameObejct.cs
+++ UnityTest/Assets/GameObejct.cs
@@ -6,7 +6,7 @@ public class GameObejct : MonoBehaviour {
     GameObejct m_cGameObject;    //test commit
 	// Use this for initialization
 	void Start () {
-        m_cGameObject.transform;
+        Transform tranform = m_cGameObject.transform;
 	}
 	
 	// Update is called once per frame
