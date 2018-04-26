b28fc40388c77fa53fbce9b19cf2d394e8914809
downkhg <37098799+downkhg@users.noreply.github.com>
2018-04-26 12:38:28 +0900

    uGUI and RoomExit

---
+++ UnityTest/Assets/Basic/ButtonTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+    public Text m_cText;
+    Button m_cButton;
+	// Use this for initialization
+	void Start () {
+        m_cButton = this.gameObject.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCilckEvent()
+    {
+        m_cText.text = "test";
+    }
+}
